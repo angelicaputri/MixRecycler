a1af2a517cbfc36da7c04e9fce9cac6af31d36da
angelicaputri <angelica.amartya01@gmail.com>
2019-03-07 18:01:57 +0700

    berhasil

---
+++ app/src/main/java/com/example/user/tugasrecycler/MainView.java
@@ -0,0 +1,9 @@
+package com.example.user.tugasrecycler;
+
+import android.content.Context;
+
+import java.util.List;
+
+public interface MainView {
+    void onSuccess(List<KampusModel> kampusModels);
+}
